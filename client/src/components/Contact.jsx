823ac829a41c9535790643ab03d0480029b66daf
MrNiNaD <ninadparkar273@gmail.com>
2024-08-02 00:39:13 +0530

    feat: formspree integration

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -17,15 +17,12 @@ const Contact = () => {
   };
 
   return (
-    <section
-      className="register-section flex-center"
-      id="contact"
-    >
+    <section className="register-section flex-center" id="contact">
       <div className="contact-container flex-center contact">
         <h2 className="form-heading">Contact Us</h2>
         <form
           method="POST"
-          action={`https://formspree.io/f/${process.env.REACT_FORMIK_SECRET}`}
+          action={`https://formspree.io/f/${process.env.REACT_APP_FORMIK_SECRET}`}
           className="register-form "
         >
           <input
@@ -55,10 +52,7 @@ const Contact = () => {
             cols="12"
           ></textarea>
 
-          <button
-            type="submit"
-            className="btn form-btn"
-          >
+          <button type="submit" className="btn form-btn">
             send
           </button>
         </form>
--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-console.log({ process: process.env })
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
